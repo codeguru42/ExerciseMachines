218101df2be0efc664d51cc509d8c2e71cb110bf
codeguru <codeguru@users.sourceforge.net>
2014-01-17 16:14:15 -0700

    Add `MachineList` and launch it when user clicks on `Browse` button

---
+++ main/src/codeguru/exercise/MachineList.java
@@ -0,0 +1,14 @@
+package codeguru.exercise;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class MachineList extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.machine_list);
+	}
+	
+}
--- main/src/codeguru/exercise/MainActivity.java
+++ main/src/codeguru/exercise/MainActivity.java
@@ -1,15 +1,25 @@
 package codeguru.exercise;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+	private static final String TAG = MainActivity.class.getName();
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void onBrowseClick(View view) {
+    	Log.d(TAG, "onBrowseClick()");
+    	Intent intent = new Intent(this, MachineList.class);
+    	startActivity(intent);
     }
     
 }
