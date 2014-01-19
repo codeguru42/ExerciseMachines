73325f969f859af8855c693b0d0e199c94a15ddc
codeguru <codeguru@users.sourceforge.net>
2014-01-19 11:04:32 -0700

    Stub out `MachineDetails` and show after scanning a bar code

---
+++ main/src/codeguru/exercise/MachineDetails.java
@@ -0,0 +1,14 @@
+package codeguru.exercise;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MachineDetails extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.machine_details);
+	}
+	
+}
--- main/src/codeguru/exercise/MainActivity.java
+++ main/src/codeguru/exercise/MainActivity.java
@@ -5,25 +5,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends Activity {
 
-	private static final String TAG = MainActivity.class.getName();
-	
+    private static final String TAG = MainActivity.class.getName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
-    
+
     public void onScanClick(View view) {
         Log.d(TAG, "onScanClick()");
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
     }
-    
+
     public void onBrowseClick(View view) {
-    	Log.d(TAG, "onBrowseClick()");
-    	Intent intent = new Intent(this, MachineList.class);
-    	startActivity(intent);
+        Log.d(TAG, "onBrowseClick()");
+        Intent intent = new Intent(this, MachineList.class);
+        startActivity(intent);
     }
-    
+
+    public void onActivityResult(int requestCode, int resultCode, Intent resultIntent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(
+                requestCode, resultCode, resultIntent);
+        if (scanResult != null) {
+            Log.d(TAG, "Barcode scanned");
+            Log.d(TAG, scanResult.toString());
+            Intent intent = new Intent(this, MachineDetails.class);
+            // Put extras to indicate which machine to load
+            startActivity(intent);
+        }
+    }
 }
