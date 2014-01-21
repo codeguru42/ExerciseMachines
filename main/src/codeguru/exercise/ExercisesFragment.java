4d29c2217f1a8991b2c56ed8464291afce92c393
codeguru <codeguru@users.sourceforge.net>
2014-01-21 12:54:36 -0700

    Stub out `ExercisesFragment`

--- main/src/codeguru/exercise/ExercisesFragment.java
+++ main/src/codeguru/exercise/ExercisesFragment.java
@@ -6,12 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ImagesFragment extends Fragment {
+public class ExercisesFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.images, parent, false);
+        return inflater.inflate(R.layout.exercises, parent, false);
     }
-    
+
 }
