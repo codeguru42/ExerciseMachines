24d56b40f58b1991e0d2d435ca633e06041e4331
codeguru <codeguru@users.sourceforge.net>
2014-01-21 12:23:57 -0700

    Add tabs to `MachineDetails`

--- main/src/codeguru/exercise/MachineDetails.java
+++ main/src/codeguru/exercise/MachineDetails.java
@@ -1,14 +1,49 @@
 package codeguru.exercise;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.Tab;
+import android.support.v7.app.ActionBar.TabListener;
 import android.support.v7.app.ActionBarActivity;
 
-public class MachineDetails extends ActionBarActivity {
+public class MachineDetails extends ActionBarActivity implements TabListener {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.machine_details);
-	}
-	
+    private static final int TAB_COUNT = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.machine_details);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        for (int i = 0; i < TAB_COUNT; ++i) {
+            ActionBar.Tab tab = actionBar.newTab();
+
+            tab.setText("Tab " + (i + 1));
+            tab.setTabListener(this);
+            actionBar.addTab(tab);
+        }
+    }
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+        // TODO Auto-generated method stub
+        
+    }
+
 }
