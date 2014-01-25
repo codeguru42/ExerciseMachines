766a1e1a7423ea1dd9f111c08b461da2ddac28b6
codeguru <codeguru@users.sourceforge.net>
2014-01-25 16:10:07 -0700

    Implement `MachineProvider`

--- main/src/codeguru/exercise/provider/MachineContract.java
+++ main/src/codeguru/exercise/provider/MachineContract.java
@@ -1,10 +1,28 @@
 package codeguru.exercise.provider;
 
+import android.content.ContentResolver;
+import android.net.Uri;
+
 public class MachineContract {
 
     public static final String DB_NAME = "machines.db";
 
     public static final int SCHEMA_VERSION = 1;
+
+    public static final String MACHINES_TABLE = "machines";
+
+    public static final String AUTHORITY = "codeguru.exercise.provider";
+
+    public static final Uri MACHINES_URI = new Uri.Builder().scheme("content")
+            .authority(AUTHORITY).path(MACHINES_TABLE).build();
+
+    public static final String MACHINE_LIST_MIME_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE
+            + "/machine";
+
+    public static final String MACHINE_ITEM_MIME_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE
+            + "/machine";
+
+    public static final String ID_COL = "_id";
 
     private MachineContract() {
     }
--- main/src/codeguru/exercise/provider/MachineProvider.java
+++ main/src/codeguru/exercise/provider/MachineProvider.java
@@ -1,49 +1,114 @@
 package codeguru.exercise.provider;
 
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.net.Uri;
-
 import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.text.TextUtils;
+import codeguru.exercise.R;
 
 public class MachineProvider extends ContentProvider {
 
+    private static final int ALL_MACHINES = 1;
+    private static final int MACHINE_ID = 2;
+
+    private static final UriMatcher uriMatcher = new UriMatcher(
+            UriMatcher.NO_MATCH);
+
+    static {
+        uriMatcher.addURI(MachineContract.AUTHORITY,
+                MachineContract.MACHINES_TABLE, ALL_MACHINES);
+        uriMatcher.addURI(MachineContract.AUTHORITY,
+                MachineContract.MACHINES_TABLE + "/#", MACHINE_ID);
+    }
+
+    private SQLiteOpenHelper dbHelper;
+
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // TODO Auto-generated method stub
-        return 0;
+        throw new UnsupportedOperationException("delete not supported");
     }
 
     @Override
     public String getType(Uri uri) {
-        // TODO Auto-generated method stub
-        return null;
+        switch (uriMatcher.match(uri)) {
+            case ALL_MACHINES:
+                return MachineContract.MACHINE_LIST_MIME_TYPE;
+
+            case MACHINE_ID:
+                return MachineContract.MACHINE_ITEM_MIME_TYPE;
+
+            default:
+                String error = this.getContext().getString(
+                        R.string.invalid_uri_error, uri.toString());
+                throw new IllegalArgumentException(error);
+        }
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        // TODO Auto-generated method stub
-        return null;
+        throw new UnsupportedOperationException("insert not supported");
     }
 
     @Override
     public boolean onCreate() {
-        // TODO Auto-generated method stub
-        return false;
+        dbHelper = new MachineDbHelper(getContext());
+        return true;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
             String[] selectionArgs, String sortOrder) {
-        // TODO Auto-generated method stub
-        return null;
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        switch (uriMatcher.match(uri)) {
+            case ALL_MACHINES:
+                return db.query(MachineContract.MACHINES_TABLE, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+
+            case MACHINE_ID:
+                String where = this.getWhereWithId(selection);
+                long id = ContentUris.parseId(uri);
+                String[] whereArgs = this.getWhereArgsWithId(selectionArgs, id);
+                return db.query(MachineContract.MACHINES_TABLE, projection,
+                        where, whereArgs, null, null, sortOrder);
+
+            default:
+                String error = this.getContext().getString(
+                        R.string.invalid_uri_error, uri.toString());
+                throw new IllegalArgumentException(error);
+        }
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection,
             String[] selectionArgs) {
-        // TODO Auto-generated method stub
-        return 0;
+        throw new UnsupportedOperationException("update not supported");
+    }
+
+    private String getWhereWithId(String selection) {
+        String idSelection = MachineContract.ID_COL + " = ?";
+        return TextUtils.isEmpty(selection) ? idSelection : idSelection
+                + " AND (" + selection + ")";
+
+    }
+
+    private String[] getWhereArgsWithId(String[] selectionArgs, long id) {
+        int argCount = selectionArgs == null ? 1 : selectionArgs.length + 1;
+        String[] whereArgs = new String[argCount];
+        whereArgs[0] = Long.toString(id);
+
+        if (selectionArgs != null) {
+            for (int i = 0; i < selectionArgs.length; ++i) {
+                whereArgs[i + 1] = selectionArgs[i];
+            }
+        }
+
+        return whereArgs;
     }
 
 }
