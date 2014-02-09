a16ad5440c04b93af8ea52520d4aadeea6597ad9
codeguru <codeguru@users.sourceforge.net>
2014-02-08 23:04:51 -0700

    Remove unnecessary repetition from SQL code

--- main/assets/create.sql
+++ main/assets/create.sql
@@ -11,39 +11,33 @@ VALUES ( 1
        , "Inclined Bench Press"
        , "file:///android_asset/desc/InclinedBench.html"
        , "img/InclinedBenchThumb.jpeg"
-       );
-INSERT INTO machines (_id, name, description, thumbnail)
-VALUES ( 2
+       ),
+       ( 2
        , "Seated Row"
        , "file:///android_asset/desc/SeatedRow.html"
        , "img/SeatedRowThumb.jpeg"
-       );
-INSERT INTO machines (_id, name, description, thumbnail)
-VALUES ( 3
+       ),
+       ( 3
        , "Seated Tricep Extension"
        , "file:///android_asset/desc/SeatedTricepExtension.html"
        , "img/TricepExtensionThumb.jpeg"
-       );
-INSERT INTO machines (_id, name, description, thumbnail)
-VALUES ( 4
+       ),
+       ( 4
        , "Leg Extension"
        , "file:///android_asset/desc/LegExtension.html"
        , "img/LegExtensionThumb.jpeg"
-       );
-INSERT INTO machines (_id, name, description, thumbnail)
-VALUES ( 5
+       ),
+       ( 5
        , "Leg Press"
        , "file:///android_asset/desc/LegPress.html"
        , "img/LegPressThumb.jpeg"
-       );
-INSERT INTO machines (_id, name, description, thumbnail)
-VALUES ( 6
+       ),
+       ( 6
        , "Hack Squat"
        , "file:///android_asset/desc/HackSquat.html"
        , "img/HackSquatThumb.jpeg"
-       );
-INSERT INTO machines (_id, name, description, thumbnail)
-VALUES ( 7
+       ),
+       ( 7
        , "Leg Curl"
        , "file:///android_asset/desc/LegCurl.html"
        , "img/LegCurlThumb.jpeg"
@@ -57,33 +51,19 @@ CREATE TABLE images
 );
 BEGIN TRANSACTION;
 INSERT INTO images (machine_id, image_uri)
-VALUES ( 1, "img/InclinedBench01.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 1, "img/InclinedBench02.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 1, "img/InclinedBench03.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 2, "img/SeatedRow01.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 2, "img/SeatedRow02.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 3, "img/TricepExtension01.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 3, "img/TricepExtension02.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 4, "img/LegExtension01.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 4, "img/LegExtension02.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 5, "img/LegPress01.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 5, "img/LegPress02.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 6, "img/HackSquat01.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 6, "img/HackSquat02.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 7, "img/LegCurl01.jpeg" );
-INSERT INTO images (machine_id, image_uri)
-VALUES ( 7, "img/LegCurl02.jpeg" );
+VALUES ( 1, "img/InclinedBench01.jpeg" ),
+       ( 1, "img/InclinedBench02.jpeg" ),
+       ( 1, "img/InclinedBench03.jpeg" ),
+       ( 2, "img/SeatedRow01.jpeg" ),
+       ( 2, "img/SeatedRow02.jpeg" ),
+       ( 3, "img/TricepExtension01.jpeg" ),
+       ( 3, "img/TricepExtension02.jpeg" ),
+       ( 4, "img/LegExtension01.jpeg" ),
+       ( 4, "img/LegExtension02.jpeg" ),
+       ( 5, "img/LegPress01.jpeg" ),
+       ( 5, "img/LegPress02.jpeg" ),
+       ( 6, "img/HackSquat01.jpeg" ),
+       ( 6, "img/HackSquat02.jpeg" ),
+       ( 7, "img/LegCurl01.jpeg" ),
+       ( 7, "img/LegCurl02.jpeg" );
 COMMIT;
