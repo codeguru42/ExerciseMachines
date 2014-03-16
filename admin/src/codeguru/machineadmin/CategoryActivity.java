fab30cbb628a9b55e14a73508f00b013f6d4a2c5
codeguru <codeguru@users.sourceforge.net>
2014-03-15 20:17:25 -0600

    MachineAdmin: Add menu with "Save" item to `CategoryActivity`

--- admin/src/codeguru/machineadmin/CategoryActivity.java
+++ admin/src/codeguru/machineadmin/CategoryActivity.java
@@ -2,6 +2,9 @@ package codeguru.machineadmin;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 import com.loopj.android.image.SmartImageView;
 
 public class CategoryActivity extends ActionBarActivity {
@@ -13,6 +16,16 @@ public class CategoryActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category);
         mThumbnail = (SmartImageView) findViewById(R.id.thumbnail);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.category, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void onSave(MenuItem menuItem) {
+        Toast.makeText(this, "Saving...(not really)", Toast.LENGTH_LONG).show();
     }
 
 }
