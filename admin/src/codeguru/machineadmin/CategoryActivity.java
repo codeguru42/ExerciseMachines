f0ea78c080b517da3e016f3574e36a6e8ce92e59
codeguru <codeguru@users.sourceforge.net>
2014-03-22 10:45:40 -0600

    MachineAdmin: Get thumbnail for category

--- admin/src/codeguru/machineadmin/CategoryActivity.java
+++ admin/src/codeguru/machineadmin/CategoryActivity.java
@@ -1,13 +1,18 @@
 package codeguru.machineadmin;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 import com.loopj.android.image.SmartImageView;
 
 public class CategoryActivity extends ActionBarActivity {
+
+    public static final int SELECT_PICTURE = 1;
 
     private SmartImageView mThumbnail;
 
@@ -26,6 +31,25 @@ public class CategoryActivity extends ActionBarActivity {
 
     public void onSave(MenuItem menuItem) {
         Toast.makeText(this, "Saving...(not really)", Toast.LENGTH_LONG).show();
+    }
+
+    public void onGetImage(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),
+                MainActivity.SELECT_PICTURE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri imageUri = data.getData();
+                getLoaderManager().initLoader(ImageLoader.IMAGE_URL_LOADER,
+                        null, new ImageLoader(this, mThumbnail, imageUri));
+            }
+        }
     }
 
 }
